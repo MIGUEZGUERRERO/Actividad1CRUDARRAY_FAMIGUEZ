48745866195d93fafca47ab7c7a3f7aea217ef70
MIGUEZGUERRERO <120044509+MIGUEZGUERRERO@users.noreply.github.com>
2023-02-13 17:25:27 +0100

    Add files via upload
    Se actualiza la versión de Menú.
    Se añade el método para inicializar datos.

---
+++ inicializarDatos.java
@@ -0,0 +1,14 @@
+
+public class inicializarDatos {
+
+	public static void inicializar(String[] lista, String dato) {
+
+		int longitud = lista.length;
+		for (int i = 0; i < longitud; i++) {
+		lista[i]=dato;		
+		}
+
+	}
+}
+
+
